26b7d49b0340d511f53cd51a43069ea02125d33c
cankocak <can_kocak1@epam.com>
2026-01-19 11:49:32 +0300

    completed Functions 🦁

---
+++ src/functions.rs
@@ -0,0 +1,172 @@
+/*
+* Functions
+*
+* Functions are prevalent in Rust code. The main function is the entry point of many programs. fn
+* keyword is used to declare new functions.
+*
+* Rust code uses snake case as the conventional style for function and variable names, in which all
+* letters all lowercase and underscores separate words.
+*
+*   fn main() {
+*       println!("Hello, world!");
+*       another_function();
+*   }
+*
+*   fn another_function() {
+*       println!("Another function.");
+*   }
+* We define a function in Rust by entering fn followed by a function name and a set of parantheses.
+* The curly brackets tell the compiler where the function body begins and ends.
+*
+* We can call any function we've defined by entering its name followed by a set of parantheses.
+* Because another_function is defined in the program, it can be called from inside the main
+* function. Note that we defined another_function after the main function in the source code; We
+* could have defined it before as well. Rust doesn't care where you define your functions, only
+* that they're defined somewhere in a scope that can be seen by the caller.
+*
+* Parameters
+*
+* We can define functions to have parameters, which are special variables that are part of a
+* function's signature. When a function has parameters, you can provide it with concrete values for
+* those parameters. Technically, the concrete values are called arguments, but in casual
+* conversation, people tend to use the words parameter and argument interchangeably for either the
+* variables in a function's definition or the concrete values passed in when you call a function.
+*
+*   fn main() {
+*       another_function(5);
+*   }
+*
+*   fn another_function(x: i32) {
+*       println!("The value of x is: {x}");
+*   }
+* The declaration of another_function has one parameter named x. The type of x is specified as i32.
+* When we pass 5 in to another_function, the println! macro puts 5 where the pair of curly brackets
+* containing x was in the format string.
+*
+* In function signatures, you must declare the type of each parameter. This is a deliberate
+* decision in Rust's design: requiring type annotations in function definitions means the compiler
+* almost never needs you to use them elsewhere in the code to figure out what type you mean. The
+* compiler is also able to give more helpful messages if it knows what types the function expects.
+*
+* When defining multiple parameters, separate the parameter declarations with commas, like this:
+*
+*   fn main() {
+*       print_labeled_measurement(5, 'h');
+*   }
+*
+*   fn print_labeled_measurement(value: i32, unit_label: char) {
+*       println!("The measurement is: {value}{unit_label}");
+*   }
+* This example creates a function named print_labeled_measurement with two parameters. The first
+* parameter is named value and is an i32. The second is named unit_label and is type char. The
+* function then prints text containing both the value and the unit_label.
+*
+* Statements and Expressions
+*
+* Function bodies are made up of a series of statements optionally ending in an expression. Because
+* Rust is an expression-based language, this is an important distinction to understand.
+*
+* - Statements are instructions that perform some action and do not return a value.
+* - Expressions evaluate to a resultant value.
+* Creating a variable and assigning a value to it with the let keyword is a statement. Function definitions are also statements. let y = 6;
+* is a statement. Statements do not return values. Therefore, you can't assign a let statement to
+* another variable.
+*
+*   let x = (let y = 6);    -> ERROR!
+* The let y = 6 statement does not return a value, so there isn't anything for x to bind to. This
+* is different from what happens in other languages, such as C and Ruby, where the assignment
+* returns the value of the assignment. In those languages, you can write x = y = 6 and have both x
+* and y have the value 6; that is not the case in Rust.
+*
+* Expressions evaluate to a value and make up most of the rest of the code that you'll write in
+* Rust. Consider a math operation, such as 5 + 6, which is an expression that evaluates to the
+* value 11. Expressions can be part of statements. Calling a function is an expression. Calling a
+* macro is an expression. A new scope block created with curly brackets is an expression, for
+* example:
+*
+*   fn main() {
+*       let y = {
+*           let x = 3;
+*           x + 1
+*       };
+*
+*       println!("The value of y is: {y}");
+*   }
+* This expression is a block that, in this case, evaluates to 4. The value gets bound to y as part
+* of the let statement. Note that the x + 1 line doesn't have a semicolon at the end. Expression do
+* not include ending semicolons. If you add a semicolon to the end of an expression, you turn it
+* into a statement, and it will then not return a value.
+*
+* Functions with Return Values
+*
+* Functions can return values to the code that calls them. We don't name return values, but we must
+* declare their type after an arrow (->). In Rust, the return value of the function is synonymous
+* with the value of the final expression in the block of body of a function. You can return early
+* from a function by using the return keyword and specifying a value, but most functions return the
+* last expression implicitly.
+*
+*   fn five() -> i32 {
+*       5
+*   }
+*
+*   fn main() {
+*       let x = five();
+*       println!("The value of x is: {x}");
+*   }
+* There are no function calls, macros or even let statements in the five function--just the number
+* 5 itself. That's a perfectly valid function in Rust. Note that the function's return type is
+*   specified too, as -> i32. The 5 in five is the function's return value, which is why the return
+*   type is i32. There are two important bits: first, the line let x = five(); shows that we're
+*   using the return value of a function to initialize a variable. Because the function five
+*   returns a 5, that line is the same as the following:
+*
+*   let x = 5;
+* Second, the five function has no parameters and defines the type of the return value. The body of
+* the function is a lonely 5 with no semicolon because it's an expression whose value we want to
+* return.
+*
+*   fn main() {
+*       let x = plus_one(5);
+*       println!("The value of x is: {x}");
+*   }
+*
+*   fn plus_one(x: i32) -> i32 {
+*       x + 1
+*   }
+* Running this code will print The value of x is: 6. But if we place a semicolon at the end of the
+* line containing x + 1, changing it from an expression to a statement, we'll get an error.
+*
+* The main error message, mismatched types, reveals the core issue with this code. The definition
+* of the function plus_one says that it will return an i32, but statements don't evaluate to a
+* value, which is expressed by (), the unit type. Therefore, nothing is returned, which contradicts
+* the function definition and results in an error.
+*/
+fn main() {
+    println!("Hello, world!");
+    another_function();
+    different_function(19);
+    print_labeled_measurement(19, 's');
+    let x = five();
+    let x = plus_one(x);
+    println!("The value of x is: {x}");
+}
+
+fn another_function() {
+    println!("Another function.");
+}
+
+fn different_function(x: i32) {
+    println!("The value of x is: {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod constants;
 mod data_types;
+mod functions;
 mod shadowing;
 mod variables_and_mutability;
 
