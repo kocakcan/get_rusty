11a117f27869e9f7c3271c8dd51c1d01e1885ddf
cankocak <can_kocak1@epam.com>
2026-01-22 20:15:08 +0300

    added examples utilizing explicit references/dereferences 😎

--- src/references_and_borrowing.rs
+++ src/references_and_borrowing.rs
@@ -124,7 +124,20 @@ fn main() {
     let x: Box<i32> = Box::new(-1);
     let x_abs1 = i32::abs(*x);
     let x_abs2 = x.abs();
+    // let x_abs3 = (*x).abs();
     assert_eq!(x_abs1, x_abs2);
+
+    let r: &Box<i32> = &x;
+    let r_abs1 = i32::abs(**r);
+    let r_abs2 = r.abs();
+    // let r_abs3 = (*r).abs();
+    assert_eq!(r_abs1, r_abs2);
+
+    let s = String::from("Hello");
+    // let s_len1 = str::len(&s);
+    let s_len2 = s.len();
+    let s_len3 = (*s).len();
+    assert_eq!(s_len2, s_len3);
 }
 
 fn greet(g1: &String, g2: &String) {
