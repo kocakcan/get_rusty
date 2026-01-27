6716c0764dc8c4fffadbe9b823ad4fadf44ebb73
cankocak <can_kocak1@epam.com>
2026-01-27 15:13:48 +0300

    practicing references and function signatures

--- src/practice.rs
+++ src/practice.rs
@@ -10,13 +10,27 @@
 //     let _s = format!("{} {}!", m1_again, m2_again);
 // }
 
+// fn main() {
+//     let m1 = String::from("Hello");
+//     let m2 = String::from("world");
+//     greet(&m1, &m2);
+//     let _s = format!("{} {}!", m1, m2);
+// }
+//
+// fn greet(g1: &String, g2: &String) {
+//     println!("{} {}!", *g1, *g2);
+// }
+
 fn main() {
-    let m1 = String::from("Hello");
-    let m2 = String::from("world");
-    greet(&m1, &m2);
-    let _s = format!("{} {}!", m1, m2);
+    let name = String::from("Seyfi");
+    // This is read-only
+    let prefix = " Jr.";
+    let name = concat(name, prefix);
+
+    println!("My name is {}", name);
 }
 
-fn greet(g1: &String, g2: &String) {
-    println!("{} {}!", *g1, *g2);
+fn concat(mut text: String, prefix: &str) -> String {
+    text.push_str(prefix);
+    text
 }
