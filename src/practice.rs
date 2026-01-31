f0bd80242d1efb5db00cba21a57c5966799cd565
cankocak <can_kocak1@epam.com>
2026-01-31 13:14:52 +0300

    added the solutions to the quiz

--- src/practice.rs
+++ src/practice.rs
@@ -44,15 +44,45 @@
 
 /* slice::join already copies the data in name into the string full
  * which avoids unnecessary copies */
-fn stringify_name_with_title(name: &Vec<String>) -> String {
-    let mut full = name.join(" ");
-    full.push_str(" Esq.");
-    full
-}
+// fn stringify_name_with_title(name: &Vec<String>) -> String {
+//     let mut full = name.join(" ");
+//     full.push_str(" Esq.");
+//     full
+// }
+//
+// fn main() {
+//     let name = vec![String::from("Can")];
+//     let first = &name[0];
+//     stringify_name_with_title(&name);
+//     println!("{}", first);
+// }
 
-fn main() {
-    let name = vec![String::from("Can")];
-    let first = &name[0];
-    stringify_name_with_title(&name);
-    println!("{}", first);
-}
+/* let s2 = *s_ref; won't compile as s_ref doesn't own the string (it's only a reference). If Rust
+* compiled this code, when the main ends, both s and s2 would be dropped and the string would've
+* been dropped twice (double-free) which is an undefined behaviour. */
+// fn main() {
+//     let s = String::from("Can");
+//     let s_ref = &s;
+//     let s2 = *s_ref;
+//     println!("{s2}");
+// }
+
+/* You have a mutable borrow (n) to part of v, and you're trying to read from another part of v
+* (v[i - 1]) at the same time. Rust can't prove that v[i] and v[i - 1] are different elements and
+* you cannot have reader and writer at the same time */
+// fn copy_to_prev(v: &mut Vec<i32>, i: usize) {
+//     let n = &mut v[i];
+//     *n = v[i - 1];
+// }
+
+// fn main() {
+//     let mut v = vec![1, 2, 3];
+//     copy_to_prev(&mut v, 1);
+// }
+
+/* name is an immutable reference and has only R permission on the String. We cannot dereference
+* name and access the String it points to. */
+// fn award_phd(name: &String) {
+//     let mut name = *name;
+//     name.push_str(", Ph.D.");
+// }
