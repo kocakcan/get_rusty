378ca280d606eaa0f9921be94cddf7a36ad915f1
cankocak <can_kocak1@epam.com>
2026-03-04 09:50:19 +0300

    enums seem so cool

--- src/enums.rs
+++ src/enums.rs
@@ -69,10 +69,15 @@
  *
  *  let loopback = IpAddr {
  *      kind: IpAddrKind::V6,
-*       address: String::from("::1"),
+ *       address: String::from("::1"),
  *  };
-* Here we've defined a struct IpAddr that has two fields: a kind field that is of type IpAddrKind
-* (the enum we defined previously) and an address field of type String.
+ * Here we've defined a struct IpAddr that has two fields: a kind field that is of type IpAddrKind
+ * (the enum we defined previously) and an address field of type String. We have two instances of
+ * this struct. The first is home, and it has the value IpAddrKind::V4 as its kind with associated address data of 127.0.0.1. The second instance is loopback. It has the other variant of IpAddrKind as its kind value, V6, and has address ::1 associated with it. We've used a struct to bundle the kind and address values together, so now the variant is associated with the value.
+ *
+ * However, representing the same concept using just an enum is more concise: rather than an enum
+ * inside a struct, we can put data directly into each enum variant. This new definition of the
+ * IpAddr enum says that both V4 and V6 variants will have associated String values:
  */
 
 #[derive(Debug)]
@@ -92,3 +97,99 @@ fn main() {
     route(IpAddrKind::V4);
     route(IpAddrKind::V6);
 }
+
+/***
+ *  enum IpAddr {
+ *      V4(String),
+ *      V6(String),
+ *  }
+ *
+ *  let home = IpAddr::V4(String::from("127.0.0.1"));
+ *  let loopback = IpAddr::V6(String::from("::1"));
+ *
+ * We attach data to each variant of the enum directly, so there is no need for an extra struct.
+ * Here, it's also easier to see another detail of how enums work: the name of each enum variant
+ * that we define also becomes a function that constructs an instance of the enum. That is,
+ * IpAddr::V4() is a function call that takes a String argument and returns an instance of the
+ * IpAddr type. We automatically get this constructor function defined as a result of defining the
+ * enum.
+ *
+ * There's another advantage to using an enum rather than a struct: each variant can have different
+ * types and amounts of associated data. Version four IP addresses will always have four numeric
+ * components that will have values between 0 and 255. If we wanted to store V4 addresses as four
+ * u8 values but still express V6 addresses as one String value, we wouldn't be able to with a
+ * struct. Enums handle this case with ease:
+ *
+ *  enum IpAddr {
+ *      V4(u8, u8, u8, u8),
+ *      V6(String),
+ *  }
+ *
+ *  let home = IpAddr::V4(127, 0, 0, 1);
+ *  let loopback = IpAddr::V6(String::from("::1"));
+ * We've shown several different ways to define data structures to store version four and version
+ * six IP addresses. However, as it turns out, wanting to store IP addresses and encode which kind
+ * they are is so common that the standard library has a definition we can use.
+ *
+ *  struct Ipv4Addr {
+ *      // --snip--
+ *  }
+ *
+ *  struct Ipv6Addr {
+ *      // --snip--
+ *  }
+ *
+ *  enum IpAddr {
+ *      V4(Ipv4Addr),
+ *      V6(Ipv6Addr),
+ *  }
+ * This code illustrates that you can put any kind of data inside an enum variant: strings, numeric
+ * types, or structs, for example. You can even include another enum. Also, standard library types
+ * are often not much more complicated than what you might come up with.
+ *
+ * Note that even though the standard library contains a definition for IpAddr, we can still create
+ * and use our own definition without conflict because we haven't brought the standard library's
+ * definition into our scope.
+ *
+ *  enum Message {
+ *      Quit,
+ *      Move { x: i32, y: i32 },
+ *      Write(String),
+ *      ChangeColor(i32, i32, i32),
+ *  }
+ * This enum has four variants with different types:
+ *  Quit: Has no data associated with it at all
+ *  Move: Has named fields, like a struct does
+ *  Write: Includes a single String
+ *  ChangeColor: Includes three i32 values
+ * Defining an enum with variants such as the one above is similar to defining different kinds of
+ * struct definitions, except the enum doesn't use the struct keyword and all the variants are
+ * grouped together under the Message type. The following structs could hold the same data that the
+ * preceding enum variant hold:
+ *
+ *  struct QuitMessage;                         -> unit struct
+ *  struct MoveMessage {
+ *      x: i32,
+ *      y: i32,
+ *  }
+ *  struct WriteMessage(String);                -> tuple struct
+ *  struct ChangeColorMessage(i32, i32, i32)    -> tuple struct
+ * But if we used the different structs, each of which has its own type, we couldn't as easily
+ * define a function to take any of these kinds of messages as we could with the Message enum
+ * defined above, which is a single type.
+ *
+ * There is one more similarity between enums and structs: just as we're able to define methods on
+ * structs using impl, we're also able to define methods on enums.
+ *
+ *  impl Message {
+ *      fn call(&self) {
+ *          -> method body would be defined here
+ *      }
+ *  }
+ *
+ *  let m = Message::Write(String::from("hello"));
+ *  m.call();
+ * The body of the method would use self to get the value that we called the method on. In this
+ * example, we've created a variable m that has the value Message::Write(String::from("hello")),
+ * and that is what self will be in the body of the call when m.call() runs.
+ */
