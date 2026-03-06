fef06305948122f0b6e477b56639e06ad1dcd604
cankocak <can_kocak1@epam.com>
2026-03-06 14:00:37 +0300

    added Class, MinionType, SpellSchool enums

--- src/hs_example.rs
+++ src/hs_example.rs
@@ -2,30 +2,74 @@ enum Card {
     Spell {
         name: String,
         mana_cost: u8,
-        spell_school: u8,
-        class: String,
+        spell_school: SpellSchool,
+        class: Class,
     },
     Minion {
         name: String,
         mana_cost: u8,
-        minion_type: String,
-        class: String,
+        minion_type: MinionType,
+        class: Class,
     },
     Location {
         name: String,
         mana_cost: u8,
-        class: String,
+        class: Class,
     },
     Weapon {
         name: String,
         mana_cost: u8,
-        class: String,
+        class: Class,
     },
     Hero {
         name: String,
         mana_cost: u8,
-        class: String,
+        class: Class,
     },
+}
+
+#[derive(Debug)]
+enum Class {
+    DeathKnight,
+    DemonHunter,
+    Druid,
+    Hunter,
+    Mage,
+    Paladin,
+    Priest,
+    Rogue,
+    Shaman,
+    Warlock,
+    Warrior,
+    Neutral,
+}
+
+#[derive(Debug)]
+enum MinionType {
+    Beast,
+    Demon,
+    Draenei,
+    Dragon,
+    Elemental,
+    Mech,
+    Murloc,
+    Naga,
+    Pirate,
+    Quilboar,
+    Totem,
+    Undead,
+}
+
+#[derive(Debug)]
+enum SpellSchool {
+    Arcane,
+    Fel,
+    Fire,
+    Frost,
+    Holy,
+    Nature,
+    Shadow,
+    General,
 }
 
 fn printc(card: Card) {
@@ -37,7 +81,7 @@ fn printc(card: Card) {
             class,
         } => {
             println!(
-                "Name: {} | Mana: {} | Spell school: {} | Class: {}",
+                "Name: {} | Mana: {} | Spell school: {:?} | Class: {:?}",
                 name, mana_cost, spell_school, class
             );
         }
@@ -48,7 +92,7 @@ fn printc(card: Card) {
             class,
         } => {
             println!(
-                "Name: {} | Mana: {} | Minion type: {} | Class: {}",
+                "Name: {} | Mana: {} | Minion type: {:?} | Class: {:?}",
                 name, mana_cost, minion_type, class
             );
         }
@@ -67,33 +111,32 @@ fn printc(card: Card) {
             mana_cost,
             class,
         } => {
-            println!("Name: {} | Mana: {} | Class: {}", name, mana_cost, class);
+            println!("Name: {} | Mana: {} | Class: {:?}", name, mana_cost, class);
         }
     }
 }
 
-// TODO: Spell school, minion type, and class should be enum
 fn main() {
     printc(Card::Minion {
         name: String::from("Edwin van Cleef"),
         mana_cost: 3,
-        minion_type: String::from("Pirate"),
-        class: String::from("Rogue"),
+        minion_type: MinionType::Pirate,
+        class: Class::Rogue,
     });
 
     printc(Card::Hero {
         name: String::from("Deathwing, Worldbreaker"),
         mana_cost: 10,
-        class: String::from("Neutral"),
+        class: Class::Neutral,
     });
     printc(Card::Weapon {
         name: String::from("Kingsbane"),
         mana_cost: 1,
-        class: String::from("Rogue"),
+        class: Class::Rogue,
     });
     printc(Card::Location {
         name: String::from("Amirdrassil"),
         mana_cost: 5,
-        class: String::from("Druid"),
+        class: Class::Druid,
     });
 }
