342a55682c73e26f5d4ddc8c3a0556452aefdf76
cankocak <can_kocak1@epam.com>
2026-03-07 16:33:09 +0300

    completed if let/let else

--- src/if_let.rs
+++ src/if_let.rs
@@ -52,6 +52,80 @@
 *   } else {
 *       count += 1;
 *   }
+*
+* Staying on the "Happy Path" with let...else
+*
+* The common pattern is to perform some computation when a value is present and return a default
+* value otherwise. Continuing on with our example of coins with a UsState value, if we wanted to
+* say something funny depending on how old the state on the quarter was, we might introduce a
+* method on UsState to check the age of a state, like so:
+*
+*   impl UsState {
+*       fn existed_in(&self, year: u16) -> bool {
+*           match self {
+*               UsState::Alabama => year >= 1819,
+*               UsState::Alaska => year >= 1959,
+*               -- snip --
+*           }
+*       }
+*   }
+* Then we might use if let to match on the type of coin, introducing a state variable within the
+* body of the condition, as shown below:
+*
+*   fn describe_state_quarter(coin: Coin) -> Option<String> {
+*       if let Coin::Quarter(state) = coin {
+*           if state.existed_in(1900) {
+*               Some(format!("{state:?} is pretty old, for America!"))
+*           } else {
+*               Some(format!({state:?} is relatively new.))
+*           }
+*       } else {
+*           None
+*       }
+*   }
+* That gets the job done, but it has pushed the work into the body of the if let statement, and if
+* the work to be done is more complicated, it might be hard to follow exactly how the top-level
+* branches relate. We could also take advantage of the fact that expressions produce a value either
+* to produce the state from the if let or to return early, as shown below:
+*
+*   fn describe_state_quarter(coin: Coin) -> Option<String> {
+*       let state = if let Coin::Quarter(state) = coin {
+*           state
+*       } else {
+*           return None;
+*       }
+*
+*       if state.existed_in(1900) {
+*           Some(format!("{state:?} is pretty old, for America!"))
+*       } else {
+*           Some(format!("{state:?} is relatively new."))
+*       }
+*   }
+* This is a bit annoying to follow in its own way, though! One branch of the if let produces a
+* value, and the other one returns from the function entirely.
+*
+* To make this common pattern nicer to express, Rust has let...else. The let...else syntax takes a
+* pattern on the left side and an expression on the right, very similar to if let, but it does not
+* have an if branch, only an else branch. If the pattern matches, it will bind the value from the
+* pattern in the outer scope. If the pattern does not match, the program will flow into the else
+* arm, which must return from the function.
+*
+*   fn describe_state_quarter(coin: Coin) -> Option<String> {
+*       let Coin::Quarter(state) = coin else {
+*           return None;
+*       }
+*
+*       if state.existed_in(1900) {
+*           Some(format!("{state:?} is pretty old, for America!"))
+*       } else {
+*           Some(format!("{state:?} is relatively new."))
+*       }
+*   }
+* Notice that it says "on the happy path" in the main body of the function this way, without having
+* significantly different control flow for two branches the way the if let did.
+*
+* If you have a situation in which your program has logic that is too verbose to express using a
+* match, remember that if let and let...else are in your Rust toolbox as well.
 */
 fn main() {
     let config_max = Some(3u8);
