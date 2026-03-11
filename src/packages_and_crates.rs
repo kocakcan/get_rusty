d690416f92df4eb63e6915270e9981dfefe6b75d
cankocak <can_kocak1@epam.com>
2026-03-11 19:40:00 +0300

    moose

--- src/packages_and_crates.rs
+++ src/packages_and_crates.rs
@@ -3,4 +3,10 @@
  * Crates: A tree of modules that produces a library or executable
  * Modules and use: Let you control the organization, scope, and privacy of paths
  * Paths: A way of naming an item, such as a struct, function, or module
+ *
+ * Packages and Crates
+ *
+ * A crate is the smallest amount of code that the Rust compiler considers at a time. Even if you
+ * run rustc rather than cargo and pass a single source code file, the compiler considers that file
+ * to be a crate.
  */
