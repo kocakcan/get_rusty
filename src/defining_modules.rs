9072a3c2f8f6e2eb7383687831dd2a0d0c778398
cankocak <can_kocak1@epam.com>
2026-03-14 03:46:23 +0300

    have to practice these explicitly

--- src/defining_modules.rs
+++ src/defining_modules.rs
@@ -20,4 +20,16 @@
  *      - Inline, directly following mod vegetables, within curly brackets instead of semicolon
  *      - In the file src/garden/vegetables.rs
  *      - In the file src/garden/vegetables/mod.rs
+*  - Paths to code in modules: Once a module is part of your crate, you can refer to code in that
+*  module from anywhere else in that same crate, as long as the privacy rules allow, using the path
+*  to the code. For example, an Asparagus type in the garden vegetables module would be found at
+*  crate::garden::vegetables::Asparagus.
+*  - Private vs. public: Code within a module is private from its parent modules by default. To
+*  make a module public, declare it with pub mod instead of mod. To make items within a public
+*  module public as well, use pub before their declarations.
+*  - The use keyword: Within a scope, the use keyword creates shortcuts to items to reduce
+*  repetition of long paths, In any scope that you can refer to
+*  crate::garden::vegetables::Asparagus, you can create a shortcut with use
+*  crate::garden::vegetables::Asparagus; and from then on you only need to write Asparagus to make
+*  use of that type in the scope.
  */
