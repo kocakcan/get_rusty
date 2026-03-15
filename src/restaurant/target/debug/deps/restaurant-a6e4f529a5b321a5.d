ba37fe2f449f3e828ac78d9fc48e269f7d943481
cankocak <can_kocak1@epam.com>
2026-03-15 11:14:34 +0300

    fuck these docs

--- src/restaurant/target/debug/deps/restaurant-a6e4f529a5b321a5.d
+++
@@ -1,9 +0,0 @@
-/Users/cankocak/get_rusty/src/restaurant/target/debug/deps/restaurant-a6e4f529a5b321a5.d: src/lib.rs Cargo.toml
-
-/Users/cankocak/get_rusty/src/restaurant/target/debug/deps/librestaurant-a6e4f529a5b321a5.rmeta: src/lib.rs Cargo.toml
-
-src/lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
-# env-dep:CLIPPY_CONF_DIR
