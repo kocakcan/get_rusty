14d6f4e192866d57c479f0ca16ddeacdb78f2afd
cankocak <can_kocak1@epam.com>
2026-03-28 13:55:08 +0300

    completed the first question

--- src/cquiz.rs
+++ src/cquiz.rs
@@ -13,7 +13,9 @@
 * the user retrieve a list of all people in a department or all people in the company by
 * department, sorted alphabetically.
 */
-fn median(v: &mut Vec<i32>) -> i32 {
+use std::collections::HashMap;
+
+fn median(v: &mut [i32]) -> i32 {
     let len = v.len();
     if len % 2 == 0 {
         (v[len / 2] + v[(len / 2) - 1]) / 2
@@ -21,9 +23,35 @@ fn median(v: &mut Vec<i32>) -> i32 {
         v[len / 2]
     }
 }
+
 fn main() {
-    let mut v: Vec<i32> = vec![1, 2, 3, 4, 10, -5, 9, 2];
+    let mut v: Vec<i32> = vec![
+        55, 34, 57, 40, 5, 29, 10, 92, 17, 76, 10, 43, 61, 97, 33, 54, 73, 81, 17, 57, 27, 72, 10,
+        96, 36, 38, 85, 95, 99, 99, 37, 50, 68, 9, 56, 10, 43, 95, 77, 66, 73, 57, 48, 2, 30, 54,
+        84, 30, 84, 88, 70, 78, 99, 12, 56, 14, 8, 81, 3, 18, 5, 39, 54, 42, 45, 51, 37, 49, 83,
+        13, 58, 88, 11, 64, 60, 66, 0, 17, 81, 82, 1, 9, 68, 94, 6, 67, 58, 42, 89, 35, 60, 13, 0,
+        65, 55, 75, 41, 59, 10, 14,
+    ];
     v.sort();
     println!("{v:?}");
     println!("Median: {}", median(&mut v));
+
+    let mut map = HashMap::new();
+
+    for num in &v {
+        map.entry(*num).and_modify(|count| *count += 1).or_insert(1);
+    }
+
+    let mut mode = 0;
+    let mut max_count = 0;
+
+    for (num, count) in &map {
+        if *count > max_count {
+            max_count = *count;
+            mode = *num;
+        }
+    }
+
+    println!("Mode: {:?}", mode);
+    // println!("{map:?}");
 }
