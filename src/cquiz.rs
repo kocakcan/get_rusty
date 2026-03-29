ab3e811da06b0958e34be207f48734ad65876d96
cankocak <can_kocak1@epam.com>
2026-03-29 17:19:50 +0300

    completed the first two questions

--- src/cquiz.rs
+++ src/cquiz.rs
@@ -24,21 +24,10 @@ fn median(v: &mut [i32]) -> i32 {
     }
 }
 
-fn main() {
-    let mut v: Vec<i32> = vec![
-        55, 34, 57, 40, 5, 29, 10, 92, 17, 76, 10, 43, 61, 97, 33, 54, 73, 81, 17, 57, 27, 72, 10,
-        96, 36, 38, 85, 95, 99, 99, 37, 50, 68, 9, 56, 10, 43, 95, 77, 66, 73, 57, 48, 2, 30, 54,
-        84, 30, 84, 88, 70, 78, 99, 12, 56, 14, 8, 81, 3, 18, 5, 39, 54, 42, 45, 51, 37, 49, 83,
-        13, 58, 88, 11, 64, 60, 66, 0, 17, 81, 82, 1, 9, 68, 94, 6, 67, 58, 42, 89, 35, 60, 13, 0,
-        65, 55, 75, 41, 59, 10, 14,
-    ];
-    v.sort();
-    println!("{v:?}");
-    println!("Median: {}", median(&mut v));
-
+fn mode(v: &[i32]) -> i32 {
     let mut map = HashMap::new();
 
-    for num in &v {
+    for num in v {
         map.entry(*num).and_modify(|count| *count += 1).or_insert(1);
     }
 
@@ -51,21 +40,34 @@ fn main() {
             mode = *num;
         }
     }
+    mode
+}
 
-    println!("Mode: {:?}", mode);
-    // println!("{map:?}");
-
-    let input = String::from("Ciaran");
-    let first = input.chars().next().unwrap();
-    let result = match first {
+fn piglatin(text: &str) -> String {
+    match text.chars().next().unwrap() {
         'a' | 'e' | 'i' | 'o' | 'u' | 'A' | 'E' | 'I' | 'O' | 'U' => {
-            format!("{input}-hay")
+            format!("{text}-hay")
         }
         c => {
-            let rest = &input[c.len_utf8()..];
+            let rest = &text[c.len_utf8()..];
             format!("{rest}-{c}ay")
         }
-    };
+    }
+}
 
-    println!("{result}");
+fn main() {
+    let mut v: Vec<i32> = vec![
+        55, 34, 57, 40, 5, 29, 10, 92, 17, 76, 10, 43, 61, 97, 33, 54, 73, 81, 17, 57, 27, 72, 10,
+        96, 36, 38, 85, 95, 99, 99, 37, 50, 68, 9, 56, 10, 43, 95, 77, 66, 73, 57, 48, 2, 30, 54,
+        84, 30, 84, 88, 70, 78, 99, 12, 56, 14, 8, 81, 3, 18, 5, 39, 54, 42, 45, 51, 37, 49, 83,
+        13, 58, 88, 11, 64, 60, 66, 0, 17, 81, 82, 1, 9, 68, 94, 6, 67, 58, 42, 89, 35, 60, 13, 0,
+        65, 55, 75, 41, 59, 10, 14,
+    ];
+    v.sort();
+    println!("Median: {}", median(&mut v));
+    println!("Mode: {:?}", mode(&v));
+
+    println!("Result: {}", piglatin(&String::from("Ciaran")));
+    println!("Result: {}", piglatin(&String::from("Artorias")));
+    // let mut company: HashMap<String, Vec<String>> = HashMap::new();
 }
