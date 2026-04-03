3d4b742c67cddf21712b60fb483bfcf9166601c6
cankocak <can_kocak1@epam.com>
2026-04-03 13:43:25 +0300

    completed the first section

--- src/error_handling.rs
+++ src/error_handling.rs
@@ -74,7 +74,54 @@
  *      let v = vec![1, 2, 3];
  *      v[99];
  *   }
+ * Here, we're attempting to access the 100th element of our vector (which is at index 99 because
+ * indexing starts at zero), but the vector has only three elements. In this situation, Rust
+ * will panic. Using [] is supposed to return an element, but if you pass an invalid index, there's
+ * no element that Rust could return here that would be correct.
+ *
+ * In C, attempting to read beyond the end of a data structure is undefined behaviour. You might
+ * get whatever is at the location in memory that would correspond to that element in the data
+ * structure, even though the memory doesn't belong to that structure. This is called a buffer
+ * overhead and can lead to security vulnerabilities if an attacker is able to manipulate the index
+ * in such a way as to read data they shouldn't be allowed to that is stored after the data
+ * structure.
+ *
+ * To protect your program from this sort of vulnerability, if you try to read an element at an
+ * index that doesn't exist, Rust will stop execution and refuse to continue.
+ *
+ *  thread 'main' panicked at src/main.rs:4:6:
+ *  index out of bounds: the len is 3 but the index is 99
+ *
+ * This error points at line 4 of our main.rs where we attempt to access index 99 of the vector in
+ * v.
+ *
+ * The note: line tells us that we can set the RUST_BACKTRACE environment variable to get a
+ * backtrace of exactly what happened to cause the error. A backtrace is a list of all the
+ * functions that have been called to get to this point. Backtraces in Rust work as they do in
+ * other languages: the key to reading the backtrace is to start from the top and read until you
+ * see files you wrote. That's the spot where the problem originated. The lines above that spot are
+ * code that your code has called; the lines below are code that called your code. These
+ * before-and-after lines might include core Rust code, standard library code, or crates that
+ * you're using. Let's try getting a backtrace by setting the RUST_BACKTRACE environment variable
+ * to any value except 0.
+ *
+ * That's a lot of output! The exact output you see might be different depending on your operating
+ * system and Rust version. In order to get backtraces with this information, debug symbols must be
+ * enabled. Debug symbols are enabled by default when using cargo build or cargo run without the
+ * --release flag, as we have here.
+ *
+ * In the output above, line 6 of the backtrace points to the line in our project that's causing
+ * the problem: line 4 of src/main.rs. If we don't want our program to panic, we should start our
+ * investigation at the location pointed to by the first line mentioning a file we wrote. Above,
+ * where we deliberately wrote code that would panic, the way to fix the panic is to not request an
+ * element beyond the range of the vector indexes. When your code panics in the future, you'll need
+ * to figure out what action the code is taking with what values to cause the panic and what the
+ * code should do instead.
+ *
+ * Recoverable Errors with Result
  */
 fn main() {
-    panic!("crash and burn");
+    // panic!("crash and burn");
+    let v = vec![1, 2, 3];
+    v[99];
 }
