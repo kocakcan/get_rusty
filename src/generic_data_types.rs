f32607425080d436afb12cd457a26bc302156ef7
cankocak <can_kocak1@epam.com>
2026-04-06 20:55:43 +0300

    about to create my first generic function

---
+++ src/generic_data_types.rs
@@ -0,0 +1,48 @@
+/// Generic Data Types
+///
+/// We use generics to create definitions for items like function signatures or structs, which we
+/// can then use with many different concrete data types.
+///
+/// In Function Definitions
+///
+/// When defining a function that uses generics, we place the generics in the signature of the
+/// function where we would usually specify the data types of the parameters and return value.
+/// Doing so makes our code more flexible and provides more functionality to callers of our
+/// function while preventing code duplication.
+///
+/// Continuing with our largest function, Listing 10-4 shows two functions that both find the
+/// largest value in a slice. We'll then combine these into a single function that uses generics.
+///
+///     fn largest_i32(list: &[i32]) -> &i32 {
+///         let mut largest = &list[0];
+///         for item in list {
+///             if item > largest {
+///                 largest = item;
+///             }
+///         }
+///         largest
+///     }
+///
+///     fn largest_char(list: &[char]) -> &char {
+///         let mut largest = &list[0];
+///         for item in list {
+///             if item > largest {
+///                 largest = item;
+///             }
+///         }
+///         largest
+///     }
+///
+///     fn main() {
+///         let number_list = vec![34, 50, 25, 100, 65];
+///         let result = largest_i32(&number_list);
+///         println!("The largest number is {result}");
+///         let char_list = vec!['y', 'm', 'a', 'q'];
+///         let result = largest_char(&char_list);
+///         println!("The largest char is {result}");
+///     }
+///     Listing 10-4: Two functions that differ only in their names in their signatures
+/// The largest_i32 function is the one we extracted in Listing 10-3 that finds the largest i32 in
+///  a slice. The largest_char function finds the largest char in a slice. The function bodies have
+///  the same code, so let's eliminate the duplication by introducing a generic type parameter in a
+///  single function.
