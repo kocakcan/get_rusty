395ef5b58189ad9970a80b6a7593e52c71462f2b
cankocak <can_kocak1@epam.com>
2026-04-10 10:21:34 +0300

    implemented my first trait

--- src/traits.rs
+++ src/traits.rs
@@ -35,3 +35,130 @@
 /// the method signatures that describes the behaviours of the types that implement this trait,
 /// which in this case is fn summarize(&self) -> String.
 ///
+/// After the method signature, instead of providing an implementation within curly brackets, we
+/// use a semicolon. Each type implementing this trait must provide its own custom behaviour for
+/// the body of the method. The compiler will enforce that any type that has the Summary trait will
+/// have the method summarize defined with this signature exactly.
+///
+/// A trait can have multiple methods in its body: the method signatures are listed one per line,
+/// and each line ends in a semicolon.
+///
+/// Implementing a Trait on a Type
+///
+/// Now that we've defined the desired signatures of the Summary trait's methods, we can implement
+/// it on the types in our media aggregator. Listing 10-13 shows an implementation of the Summary
+/// trait on the NewsArticle struct that uses the headline, the author, and the location to create
+/// the return value of summarize. For the SocialPost struct, we define summarize as the username
+/// followed by the entire text of the post, assuming that the post content is already limited to
+/// 280 characters.
+///
+///     pub struct NewsArticle {
+///         pub headline: String,
+///         pub location: String,
+///         pub author: String,
+///         pub content: String,
+///     }
+///
+///     impl Summary for NewsArticle {
+///         fn summarize(&self) -> String {
+///             format!("{}, by {} ({})", self.headline, self.author, self.location)
+///         }
+///     }
+///
+///     pub struct SocialPost {
+///         pub username: String,
+///         pub content: String,
+///         pub reply: bool,
+///         pub repost: bool,
+///     }
+///
+///     impl Summary for SocialPost {
+///         fn summarize(&self) -> String {
+///             format!("{}: {}", self.username, self.content)
+///         }
+///     }
+///     Listing 10-13: Implementing the Summary trait on the NewsArticle and SocialPost types
+/// Implementing a trait on a type is similar to implementing regular methods. The difference is
+/// that after impl, we put the trait name we want to implement, then use the for keyword,and then
+/// specify the name of the type we want to implement the trait for. Within the impl block, we put
+/// the method signatures that the trait definition has defined. Instead of adding a semicolon
+/// after each signature, we use curly brackets and fill in the method body with the specific
+/// behaviour that we want the methods of the trait to have for the particular type.
+///
+/// Now that the library has implemented the Summary trait on NewsArticle and SocialPost, users of
+/// the crate can call the trait methods on instances of NewsArticle and SocialPost in the same way
+/// we call regular methods. The only difference is that the user must bring the trait into scope
+/// as well as the types. Here's an example of how a binary crate could use our aggregator library
+/// crate:
+///
+///     use aggregator::{SocialPost, Summary};
+///
+///     fn main() {
+///         let post = SocialPost {
+///             username: String::from("horse_ebooks"),
+///             content: String::from(
+///                 "of course, as you probably already know, people",
+///             ),
+///             reply: false,
+///             repost: false,
+///         }
+///
+///         println!("1 new post: {}", post.summarize());
+///     }
+/// This code prints 1 new post: horse_ebooks: of course, as you probably already know, people.
+///
+/// Other crates that depend on the aggregator crate can also bring the Summary trait into scope to
+/// implement Summary on their own types. One restriction to note is that we can implement a trait
+/// on a type only if either the trait or the type, or both, are local to our crate. For example,
+/// we can implement standard library traits like Display on a custom type like SocialPost as part
+/// of our aggregator crate functionality because the type SocialPost is local to our aggregator
+/// crate. We can also implement Summary on Vec<T> in our aggregator crate because the trait
+/// Summary is local to our aggregator crate.
+///
+/// But we can't implement external traits on external types. For example, we can't implement the
+/// Display trait on Vec<T> within our aggregator crate because Display and Vec<T> are both defined
+/// in standard library and aren't local to our aggregator crate. This restriction is part of a
+/// property called coherence, and more specifically the orphan rule, so named because the parent
+/// type is not present. This rule ensures that other people's code can't break your code and vice
+/// versa. Without the rule, two crates could implement the same trait for the same type, and Rust
+/// wouldn't know which implementation to use.
+pub trait Summary {
+    fn summarize(&self) -> String;
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct SocialPost {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub repost: bool,
+}
+
+impl Summary for SocialPost {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
+fn main() {
+    let post = SocialPost {
+        username: String::from("galatasaray"),
+        content: String::from("Osimhen will be back before the big derby!"),
+        reply: false,
+        repost: false,
+    };
+
+    println!("1 new post: {}", post.summarize());
+}
