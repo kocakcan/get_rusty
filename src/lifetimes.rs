c45a1bfa78e4e28ec6f6f000ebe1cf5b010e1a53
cankocak <can_kocak1@epam.com>
2026-04-17 13:06:42 +0300

    completed thinking in terms of lifetimes

--- src/lifetimes.rs
+++ src/lifetimes.rs
@@ -249,8 +249,62 @@
 /// possibly having an invalid reference.
 ///
 /// Thinking in Terms of Lifetimes
+///
+/// The way in which you need to specify lifetime parameters depends on what your function is
+/// doing. For example, if we changed the implementation of the longest function to always return
+/// the first parameter rather than the longest string slice, we wouldn't need to specify a
+/// lifetime on the y parameter. The following code will compile:
+///
+///     fn longest<'a>(x: &'a str, y: &str) -> &'a str {
+///         x
+///     }
+/// We've specified a lifetime parameter 'a for the parameter x and the return type, but not for
+/// the parameter y, because the lifetime of y does not have any relationship with the lifetime of
+/// x or the return value.
+///
+/// When returning a reference froma function, the lifetime parameter for the return type needs to
+/// match the lifetime parameter for one of the parameters. If the reference returned does not
+/// refer to one of the parameters, it must refer to a value created within this function. However,
+/// this would be a dangling reference because the value will go out of scope at the end of the
+/// function. Consider this attempted implementation of the longest function that won't compile:
+///
+///     fn longest<'a>(x: &str, y: &str) -> &'a str {
+///         let result = String::from("really long string");
+///         result.as_str()
+///     }
+/// Here, even though we've specified a lifetime parameter 'a for the return type, this
+/// implementation will fail to compile because the return value lifetime is not related to the
+/// lifetime of the parameters at all. Here is the error message we get:
+///
+///     error[E0515]: cannot return value referencing local variable `result`
+/// The problem is that result goes out of scope and gets cleaned up at the end of the longest
+/// function. We've also trying to return a reference to result from the function. There is no way
+/// we can specify lifetime parameters that would change the dangling reference, and Rust won't let
+/// us create a dangling reference. In this case, the best fix would be to return an owned data
+/// type rather than a reference so the calling function is then responsible for cleaning up the value.
+///
+/// Ultimately, lifetime syntax is about connecting the lifetimes of various parameters and return
+/// values of functions. Once they're connected, Rust has enough information to allow memory-safe
+/// operations and disallow operations that would create dangling pointers or otherwise violate
+/// memory safety.
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() { x } else { y }
+}
+
+enum AttackType {
+    Light,
+    Normal,
+    Heavy,
+    ChargedHeavy
+}
+
+fn get_damage(at: &AttackType) -> u32 {
+    match at {
+        AttackType::Light => 10,
+        AttackType::Normal => 25,
+        AttackType::Heavy => 50,
+        AttackType::ChargedHeavy => 100,
+    }
 }
 
 fn main() {
@@ -258,5 +312,7 @@ fn main() {
     let string2 = "xyz";
 
     let result = longest(string1.as_str(), string2);
+    let first_attack = AttackType::ChargedHeavy;
     println!("The longest string is {result}");
+    println!("I will do {} damage to you", get_damage(&first_attack));
 }
