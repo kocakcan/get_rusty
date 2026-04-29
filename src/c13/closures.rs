16e32a3713ec6d49049ae2afc18c2f87ec659863
cankocak <can_kocak1@epam.com>
2026-04-29 16:24:35 +0300

    can add type annotations to closures, can skip braces if the closure body only contains 1 expression

--- src/c13/closures.rs
+++ src/c13/closures.rs
@@ -46,6 +46,73 @@
 /// closure captures an immutable reference to the self Inventory instance and passes it with the
 /// code we specify to the unwrap_or_else method. Function, on the other hand, are not able to
 /// capture their environment in this way.
+///
+/// Closure Type Inference and Annotation
+///
+/// There are more differences between functions and closures. Closures don't usually require you
+/// to annotate the types of the parameters or the return values like fn function do. Type
+/// annotations are required on functions because the types are part of an explicit interface
+/// exposed to your users. Defining this interface rigidly is important for ensuring that everyone
+/// agrees on what type of values a function uses and returns. Closures, on the other hand, aren't
+/// used in an exposed interface like this: they're stored in variables and used without naming
+/// them and exposing them to users of our library.
+///
+/// Closures are typically short and relevant only within a narrow context rather than in any
+/// arbitrary scenario. Within these limited contexts, the compiler can infer the types of the
+/// parameters and the return type, similar to how it's able to infer the types of most variables
+/// (there are rare cases where the compiler need closure type annotations too).
+///
+/// As with variables, we can add type annotations if we want to increase explicitness and clarity
+/// at the cost of being more verbose than is strictly necessary. Annotating the types for a
+/// closure would look like the definition shown in Listing 13-2. In this example, we're defining a
+/// closure and storing it in a variable rather than defining the closure in the spot we pass it as
+/// an argument as we did in Listing 13-1.
+///
+///     let expensive_closure = |num: u32| -> u32 {
+///         println!("calculating slowly...");
+///         thread::sleep(Duration::from_secs(2));
+///         num
+///     };
+///     Listing 13-2: Adding optional type annotations of the parameter and return value types in
+///     the closure
+/// With type annotations added, the syntax of closures look more similar to the syntax of
+/// functions. Here we define a function that adds 1 to its parameter and a closure that has the
+/// same behaviour, for comparison. We've added some spaces to line up the relevant parts. This
+/// illustrates how closure syntax is similar to function syntax except for the use of pipes and
+/// the amount of syntax that is optional:
+///
+///     fn  add_one_v1   (x: i32) -> u32 { x + 1 }
+///     let add_one_v2 = |x: u32| -> u32 { x + 1 };
+///     let add_one_v3 = |x|             { x + 1 };
+///     let add_one_v4 = |x|               x + 1  ;
+/// The first line shows a function definition, and the second line shows a fully annotated closure
+/// definition. In the third line, we remove the type annotations from the closure definition. In
+/// the fourth line, we remove the brackets, which are optional because the closure body has only
+/// one expression. These are all valid definitions that will produce the same behaviour when
+/// they're called. The add_one_v3 and add_one_v4 lines require the closures to be evaluated to be
+/// able to compile because the types will be inferred from their usage. This is similar to let v =
+/// Vec::new(); needing either type annotation or values of some type to be inserted into the Vec
+/// for Rust to be able to infer the type.
+///
+/// For closure definitions, the compiler will infer one concrete type for each of their parameters
+/// and for their return value. For instance, Listing 13-3 shows the definition of a short closure
+/// that just returns the value it receives as a parameter. This closure isn't very useful except
+/// for the purposes of this example. Note that we haven't added any type annotations to the
+/// definition. Because there are no type annotations, we can call the closure with any type, which
+/// we've done here with String the first time. If we then try to call example_closure with an
+/// integer, we'll get an error.
+///
+///     let example_closure = |x| x;
+///
+///     let s = example_closure(String::from("hello"));
+///     let n = example_closure(5);
+///     Listing 13-3: Attempting to call a closure whose types are inferred with two different types
+/// The compiler give us this error:
+///
+///     error: mismatched types
+/// The first time we call example_closure with the String value, the compiler infers the type of x
+/// and the return type of the closure to be String. Those types are then locked into the closure
+/// in example_closure, and we get a type error when we try to use a different type with the same closure.
 #[derive(Debug, PartialEq, Copy, Clone)]
 enum ShirtColor {
     Red,
@@ -95,5 +162,4 @@ fn main() {
     println!("
         The user with preference {:?} gets {:?}",
         user_pref2, giveaway2);
-
 }
