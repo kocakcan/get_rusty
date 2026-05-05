ae85052db3d1b20aedb88aba23bbf3ccb9911ab1
cankocak <can_kocak1@epam.com>
2026-05-05 22:14:44 +0300

    cargo doc creates the HTML doc

---
+++ src/c14/publishing_a_crate.rs
@@ -0,0 +1,37 @@
+/// Making Useful Documentation Comments
+///
+/// Accurately documenting your packages will help other users know how and when to use them, so
+/// it's worth investing the time to write documentation. Rust has a particular kind of comments
+/// for documentation, known conveniently as a documentation comment, that will generate HTML
+/// documentation. The HTML displays the contents of documentation comments for public API items
+/// intended for programmers interested in knowing how to use your crate as opposed to how your
+/// crate is implemented.
+///
+/// Documentation comments use three slashes (///), instead of two and support Markdown notation
+/// for formatting the text. Place documentation comments just before the item they're documenting.
+/// Listing 14-1 shows documentation comments for an add_one function in a crate named my_crate.
+
+/// Adds one to the number given.
+///
+/// # Examples
+///
+/// ```
+/// let arg = 5;
+/// let answer = my_crate::add_one(arg);
+///
+/// assert_eq!(6, answer);
+/// ```
+///     pub fn add_one(x: i32) -> i32 {
+///         x + 1
+///     }
+///     Listing 14-1: A documentation comment for a function
+/// Here, we give a description of what the add_one function does, start a section with the heading
+/// Examples, and then provide code that demonstrates how to use the add_one function. We can
+/// generate the HTML documentation from this documentation comment by running cargo doc. This
+/// comment runs the rustdoc tool distributed with Rust and puts the generated HTML documentation
+/// in the target/doc directory.
+///
+/// For convenience, running cargo doc --open will build the HTML for your current crate's
+/// documentation (as well as the documentation for all of your crate's dependencies) and open the
+/// result in a browser. Navigate to the add_one function and you'll see how the text in the
+/// documentation comments is rendered.
