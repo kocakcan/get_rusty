456e26175ce179ef015edc7f70057efc6516e18c
cankocak <can_kocak1@epam.com>
2026-05-09 04:09:27 +0300

    completed second section

--- src/c15/treating_smart_pointers.rs
+++ src/c15/treating_smart_pointers.rs
@@ -190,6 +190,27 @@
 // runtime penalty for taking advantage of deref coercion!
 //
 // Handling Deref Coercion with Mutable References
+//
+// Similar to how you use the Deref trait to override the * operator on immutable references, you
+// can use the DerefMut trait to override the * operator on mutable references.
+//
+// Rust deref coercion when it finds types and trait implementations in three cases:
+//
+//  1. From &T to &U when T: Deref<Target=U>
+//  2. From &mut T to &mut U when T: DerefMut<Target=U>
+//  3. From &mut T to &U when T: Deref<Target=U>
+// The first two cases are the same except that the second implements mutability. The first case
+// states that if you have a &T, and T implements Deref to some type U, you can get a &U
+// transparently. The second case states that the same deref coercion happens for mutable references.
+//
+// The third case is trickier: Rust will also coerce a mutable reference to an immutable one. But
+// the reverse is not possible: Immutable references will never coerce to mutable references.
+// Because of the borrowing rules, if you have a mutable reference, that mutable reference must be
+// the only reference to that data (otherwise, the program wouldn't compile). Converting one mutable
+// reference to one immutable reference will never break the borrowing rules. Converting an
+// immutable reference to a mutable reference would require that initial immutable reference is the
+// only immutable reference to that data, but the borrowing rules don't guarantee that. Therefore,
+// Rust can't make the assumption that converting an immutable reference to a mutable reference is possible.
 use std::ops::Deref;
 
 struct MyBox<T>(T);
